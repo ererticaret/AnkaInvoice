b6cd92bf09841345e7db6abfbe8288e04f478266
ererticaret <erdigannnkursatmke@gmail.com>
2026-04-16 19:30:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function startApp() {
+  document.querySelector(".hero").style.display = "none";
+  document.getElementById("app").classList.remove("hidden");
+}
+
 function generateInvoice() {
   const client = document.getElementById("clientName").value;
   const service = document.getElementById("service").value;
@@ -25,4 +30,37 @@ Status: Unpaid
   `;
 
   document.getElementById("invoiceOutput").innerText = invoiceText;
+}
+
+function downloadInvoice() {
+  const { jsPDF } = window.jspdf;
+
+  const client = document.getElementById("clientName").value;
+  const service = document.getElementById("service").value;
+  const amount = document.getElementById("amount").value;
+
+  if (!client || !service || !amount) {
+    alert("Create invoice first");
+    return;
+  }
+
+  const date = new Date().toLocaleDateString();
+  const invoiceNumber = Math.floor(Math.random() * 100000);
+
+  const doc = new jsPDF();
+
+  doc.setFontSize(18);
+  doc.text("INVOICE", 20, 20);
+
+  doc.setFontSize(12);
+  doc.text(`Invoice No: ${invoiceNumber}`, 20, 40);
+  doc.text(`Date: ${date}`, 20, 50);
+
+  doc.text(`Client: ${client}`, 20, 70);
+  doc.text(`Service: ${service}`, 20, 80);
+  doc.text(`Amount: €${amount}`, 20, 90);
+
+  doc.text("Status: Unpaid", 20, 110);
+
+  doc.save("invoice.pdf");
 }
